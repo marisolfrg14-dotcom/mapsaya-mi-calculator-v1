6818896cd687d8894e17535bdacd05bad2ab7b8a
marisolfrg14-dotcom <marisolfrg14@gmail.com>
2025-10-02 08:30:18 -0600

    v1.1
    null

---
+++ multiplicacion.py
@@ -0,0 +1,7 @@
+def multi (a, b):
+    return a * b
+num1 = float(input("Ingtresa el primer numero:"))
+num2 = float(input("Ingresa el segundo numero:"))
+
+resultado = multi(num1, num2)
+print ("La multiplicacion es:", resultado)
