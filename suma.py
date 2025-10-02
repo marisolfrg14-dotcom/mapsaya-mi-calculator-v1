08e13aebb241c034eab627ec298b14c3de7bb241
marisolfrg14-dotcom <marisolfrg14@gmail.com>
2025-10-02 08:53:12 -0600

    suma
    suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+def sum (a, b):
+    return a + b
+num1 = float(input("Ingtresa el primer numero:"))
+num2 = float(input("Ingresa el segundo numero:"))
+
+resultado = sum(num1, num2)
+print ("La suma es:", resultado)
