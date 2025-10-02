c5e0a61f39b54b731d562e3965ebc5c6f9b19917
AlejandraMaldonado5T11 <237020040.itic@otemexico.tecnm.mx>
2025-10-02 08:41:55 -0600

    division operation
    This version contains the two-digit division function.

---
+++ division.py
@@ -0,0 +1,7 @@
+def divi (a, b):
+    return a / b
+num1 = float(input("Ingtresa el primer numero:"))
+num2 = float(input("Ingresa el segundo numero:"))
+
+resultado = divi(num1, num2)
+print ("La division es:", resultado)
